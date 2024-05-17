285eef17e9816b9787fb90dda840bb2d7fb6e368
Brian-Machiestay <brianmachiestay@gmail.com>
2024-05-17 06:42:03 +0000

    begin pokecard design implementation

---
+++ src/components/pokecard.jsx
@@ -0,0 +1,12 @@
+import react from 'react';
+
+
+const Pokecard = () => {
+    return (
+        <div>
+            this is a card
+        </div>
+    )
+}
+
+export default Pokecard;
--- src/pages/pokeList.jsx
+++ src/pages/pokeList.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 
 import Navbar from "../components/navbar";
+import Pokecard from "../components/pokecard";
 
+import styles from '../assets/styles/pages/pokeList.module.scss';
 
 const PokeList = () => {
     return (
-        <div>
+        <div className={styles.container}>
             <Navbar />
+            <div className={styles.poke_list}>
+                <Pokecard />
+                <Pokecard />
+            </div>
         </div>
     )
 }
